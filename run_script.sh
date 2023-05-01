ca979b9efdc02998437cac22fc8284695986f95f
Alexander Michels <michels9@illinois.edu>
2023-05-01 10:55:14 -0500

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -3,4 +3,4 @@ BASE=$(pwd)
 export SINGCVMFS_REPOSITORIES="grid.cern.ch,cybergis.illinois.edu"
 tmp_path="/tmp/cvmfs-$(openssl rand -hex 32)"
 mkdir $tmp_path
-./singcvmfs -s exec -B $tmp_path:/tmp/cvmfs,$BASE/script:/script,$BASE/output:/output -cip docker://centos:7 /cvmfs/cybergis.illinois.edu/software/conda/cjw/wrfhydro-2022-03/bin/python /script/pi.py
+./singcvmfs -s exec -B $tmp_path:/tmp/cvmfs,$BASE/script:/script,$BASE/output:/output -cip docker://cybergisx/compute-cvmfs:0.1.0 /cvmfs/cybergis.illinois.edu/software/conda/cjw/wrfhydro-2022-03/bin/python /script/pi.py

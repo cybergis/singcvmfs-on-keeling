3ee1660ad81a5c45c578bf86a7508a268fb88558
Alexander Michels <michels9@illinois.edu>
2023-05-04 10:13:49 -0500

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -2,5 +2,7 @@
 BASE=$(pwd)
 export SINGCVMFS_REPOSITORIES="grid.cern.ch,cybergis.illinois.edu"
 tmp_path="/tmp/cvmfs-$(openssl rand -hex 32)"
-srun bash -c "mkdir $tmp_path && ./singcvmfs -s exec -B $tmp_path:/tmp/cvmfs,$BASE/script:/script,$BASE/output:/output -cip docker://cybergisx/compute-cvmfs:0.1.0 /cvmfs/cybergis.illinois.edu/software/conda/cjw/wrfhydro-2022-03/bin/python /script/pi.py"
+mkdir $tmp_path
+source ~/.bashrc
 
+singcvmfs -s exec -B $tmp_path:/tmp/cvmfs,$BASE/script:/script,$BASE/output:/output -cip ~/simages/cvmfs.sif bash -c "cd /script && /cvmfs/cybergis.illinois.edu/software/conda/cjw/wrfhydro-2022-03/bin/python import.py"

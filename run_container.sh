2b30f999b34f11b278239aec81a76ec498b65581
Alexander Michels <michels9@illinois.edu>
2023-05-04 10:00:03 -0500

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -2,4 +2,6 @@
 export SINGCVMFS_REPOSITORIES="grid.cern.ch,cybergis.illinois.edu"
 tmp_path="/tmp/cvmfs-$(openssl rand -hex 32)"
 mkdir $tmp_path
-./singcvmfs -s exec -B $tmp_path:/tmp/cvmfs -cip docker://cybergisx/compute-cvmfs:0.1.0 bash
+source ~/.bashrc
+
+singcvmfs -s exec -B $tmp_path:/tmp/cvmfs,$BASE/script:/script,$BASE/output:/output -cip ~/simages/cvmfs.sif bash -c "cd /script && /cvmfs/cybergis.illinois.edu/software/conda/cjw/wrfhydro-2022-03/bin/python import.py"
